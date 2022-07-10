3f181245a5fe13c64d76c96148e5020b97ade6d7
Fumihiko Terui <25654992+fterui@users.noreply.github.com>
2022-07-10 09:19:26 +0000

    introduce redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import { Box, Button, Link, Stack } from "@mui/material";
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
+import { decrement, increment } from "./counterSlice";
 
 function App() {
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+  
   return (
     <Stack direction="row" spacing={3} className={"App"}>
-      <Box>Hello World!</Box>
-      <Button variant="contained">Click</Button>
+      <Box>Count: {count}</Box>
+      <Button variant="contained" onClick={() => dispatch(increment())}>+1</Button>
+      <Button variant="contained" onClick={() => dispatch(decrement())}>-1</Button>
       <Link href="/menu">Menu</Link>
     </Stack>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,17 @@ import "./index.css";
 import App from "./App";
 import NoRoute from "./NoRoute";
 import Menu from "./Menu";
+import { Provider } from "react-redux";
+import { store } from "./store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App/>} />
-      <Route path="/menu" element={<Menu/>} />
-      <Route path="*" element={<NoRoute/>} />
-    </Routes>
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App/>} />
+        <Route path="/menu" element={<Menu/>} />
+        <Route path="*" element={<NoRoute/>} />
+      </Routes>
+    </BrowserRouter>
+  </Provider>
 );
